c4396e410329a8a28453d677ebee504e326faff7
razgrinblat <raznadavg@gmail.com>
2024-11-14 19:48:00 +0200

    starting to add stateful tcp session tracker

---
+++ SessionTable.cpp
@@ -0,0 +1,109 @@
+#include "SessionTable.hpp"
+
+#include <thread>
+
+SessionTable::SessionTable(): _lru_list(MAX_SESSIONS),_stop_flag(true)
+{
+    _clean_up_thread = std::thread(&SessionTable::runCleanUpThread, this);
+}
+
+void SessionTable::cleanUpIdleSessions()
+{
+    const auto current_time = std::chrono::steady_clock::now();
+    std::lock_guard<std::mutex> lock_guard(_cache_mutex);
+    for (auto it = _session_cache.begin() ; it !=_session_cache.end();)
+    {
+        const std::unique_ptr<TcpSession> session = std::move(it->second);
+        const auto time_diff = std::chrono::duration_cast<std::chrono::seconds>(current_time - session->last_active_time).count();
+        if(time_diff >= MAX_IDLE_SESSION_TIME)
+        {
+            _session_cache.erase(it->first);
+            _lru_list.eraseElement(it->first);
+        }
+        else {
+            break;
+        }
+    }
+}
+
+SessionTable::~SessionTable()
+{
+    _stop_flag.store(false);
+    if (_clean_up_thread.joinable()) {
+        _clean_up_thread.join();
+    }
+    _session_cache.clear();
+}
+
+SessionTable& SessionTable::getInstance()
+{
+    static SessionTable instance;
+    return instance;
+}
+
+void SessionTable::runCleanUpThread()
+{
+    while (_stop_flag.load()) {
+        std::this_thread::sleep_for(std::chrono::seconds(CLEANUP_IDLE_SESSIONS_TIME));
+        cleanUpIdleSessions();
+    }
+}
+
+bool SessionTable::isSessionExists(const uint32_t session_hash)
+{
+    std::lock_guard<std::mutex> lock_guard(_cache_mutex);
+    const auto it = _session_cache.find(session_hash);
+    return it != _session_cache.end();
+}
+
+bool SessionTable::addNewSession(const uint32_t session_hash, std::unique_ptr<TcpSession> session, const TcpState& current_state)
+{
+    if(!isSessionExists(session_hash))
+    {
+        uint32_t session_key_to_close;
+        {
+            std::lock_guard<std::mutex> lock_guard(_cache_mutex);
+            const int result = _lru_list.put(session_hash,&session_key_to_close);
+            if(result == 1)
+            {
+                _session_cache.erase(session_key_to_close); //session cache is full. need to delete the least active connection
+            }
+            session->last_active_time = std::chrono::steady_clock::now();
+            session->current_state = current_state;
+            _session_cache[session_hash] = std::unique_ptr<TcpSession>(std::move(session));
+        }
+        return true;
+    }
+    return false;
+}
+
+bool SessionTable::closeSession(const uint32_t session_hash)
+{
+    if(isSessionExists(session_hash))
+    {
+        {
+            std::lock_guard<std::mutex> lock_guard(_cache_mutex);
+            _session_cache.erase(session_hash);
+            _lru_list.eraseElement(session_hash);
+        }
+        return true;
+    }
+    return false;
+
+}
+
+TcpState& SessionTable::getCurrentState(const uint32_t session_hash)
+{
+    if(isSessionExists(session_hash)) {
+        std::lock_guard<std::mutex> lock_guard(_cache_mutex);
+        return _session_cache[session_hash]->current_state;
+    }
+}
+
+void SessionTable::updateSession(const uint32_t session_hash, const TcpState& new_state)
+{
+    if(isSessionExists(session_hash)) {
+        std::lock_guard<std::mutex> lock_guard(_cache_mutex);
+        _session_cache[session_hash]->current_state = new_state;
+    }
+}
---
+++ SessionTable.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <IpAddress.h>
+#include <unordered_map>
+#include <LRUList.h>
+#include <memory>
+#include <chrono>
+#include <mutex>
+#include "Config.hpp"
+#include <atomic>
+#include <thread>
+
+enum TcpState {
+    SYN_SENT, SYN_RECEIVED, ESTABLISHED, FIN_WAIT1, FIN_WAIT2,
+    CLOSE_WAIT, CLOSING, TIME_WAIT, LAST_ACK
+};
+struct TcpSession
+{
+    TcpState current_state;
+    pcpp::IPv4Address source_ip;
+    pcpp::IPv4Address dst_ip;
+    uint16_t source_port;
+    uint16_t dst_port;
+    std::chrono::time_point<std::chrono::steady_clock> last_active_time;
+};
+
+class SessionTable
+{
+private:
+    std::unordered_map<uint32_t,std::unique_ptr<TcpSession>> _session_cache;
+    pcpp::LRUList<uint32_t> _lru_list;
+    std::mutex _cache_mutex;
+    std::atomic<bool> _stop_flag;
+    std::thread _clean_up_thread;
+
+    SessionTable();
+    void cleanUpIdleSessions();
+
+public:
+    ~SessionTable();
+    SessionTable(const SessionTable&) = delete;
+    SessionTable& operator=(const SessionTable&) = delete;
+    static SessionTable& getInstance();
+
+    void runCleanUpThread();
+    bool isSessionExists(uint32_t session_hash);
+    bool addNewSession(uint32_t session_hash, std::unique_ptr<TcpSession> session, const TcpState& current_state);
+    bool closeSession(uint32_t session_hash);
+    TcpState& getCurrentState(uint32_t session_hash);
+    void updateSession(uint32_t session_hash, const TcpState& new_state);
+
+
+
+
+};
+
